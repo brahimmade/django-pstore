a13ce41e90c056555858eb5e82215477a157b36b
makispal <guitarcover26@hotmail.com>
2022-10-16 02:11:19 +0300

    feat: user register login password reset

--- pstore/urls.py
+++ pstore/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("psadmin/", admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "PStore GR Admin"
