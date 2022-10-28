d75fcf9f9b1514026d7e41ae5b07d49bcc02dcda
makispal <guitarcover26@hotmail.com>
2022-10-28 22:21:40 +0300

    chore: setup docker with compose

--- pstore/urls.py
+++ pstore/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -26,7 +26,9 @@ urlpatterns = [
     path("api/v1/articles/", include("apps.articles.urls")),
     path("api/v1/ratings/", include("apps.ratings.urls")),
     path("api/v1/enquiries/", include("apps.enquiries.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# For local serving static files
+# static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "PStore GR Admin"
 admin.site.site_title = "PStore GR Admin Portal"
